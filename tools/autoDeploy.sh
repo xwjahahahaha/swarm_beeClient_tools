7657a21b10673983718fa45366c383d9ec1a84e8
xwjahahahaha <1438575858@qq.com>
2021-06-01 09:14:49 +0800

    bee_0.6.2

--- tools/autoDeploy.sh
+++ tools/autoDeploy.sh
@@ -6,16 +6,16 @@
 # 参数五 执行版本
 # 参数六 系统类型
 
-Path=./              # 工作目录
-EngineFile=bee_$5        # 启动bee执行文件名
-InitPort=10000       # 开始端口
+Path=./               # 工作目录
+EngineFile=bee_$5     # 启动bee执行文件名
+InitPort=1633         # 开始端口
 BeeYamlVersion=bee_$5.yaml
 
 [ -d $Path ]
 int=$1
-sudo rm -rf accounts.txt
+rm -rf accounts.txt
 # 结束之前的所有启动进程
-sudo ps -aux | grep $EngineFile | awk '{print $2 }' | xargs sudo kill -9
+ps -aux | grep $EngineFile | awk '{print $2 }' | xargs sudo kill -9
 while (($int <= $2)); do
     filename="bee_"$int""
     if [ ! -d $Path/$filename ]; then
@@ -27,7 +27,7 @@ while (($int <= $2)); do
         echo "配置文件不存在"
         exit
     fi
-    sudo cp ../configs/$BeeYamlVersion ./
+     cp ../configs/$BeeYamlVersion ./
     # 修改
     let newPort="InitPort+(int*10)"
     sed -i "1c api-addr: :$newPort" $BeeYamlVersion
@@ -36,8 +36,8 @@ while (($int <= $2)); do
     sed -i "4c password: \"$3\"" $BeeYamlVersion
     sed -i "5c swap-endpoint: $4" $BeeYamlVersion
     # 启动
-    sudo rm -rf output.log
-    sudo ../cmd/$6/$EngineFile start --config ./$BeeYamlVersion > output.log &
+    rm -rf output.log
+    ../cmd/$6/$EngineFile start --config ./$BeeYamlVersion > output.log &
     # 输出地址
     if [ -d .bee ]; then
         echo "$int账户已创建"
@@ -46,7 +46,7 @@ while (($int <= $2)); do
         sleep 5
     fi
     echo "$int节点账户信息:" >>../accounts.txt
-    sudo ../exportKeys/exportKeys_$6.exe .bee/keys/ $3 | awk 'NR==3{print $3}' >>../accounts.txt
+    ../exportKeys/exportKeys_$6.exe .bee/keys/ $3 | awk 'NR==3{print $3}' >>../accounts.txt
     echo "============================================================" >>../accounts.txt
     echo "$int节点账户导出成功!"
     cd ..
